0a21790720a2b5ce266b532a904b1e52de359c6d
kenharis93 <41925865+kenharis93@users.noreply.github.com>
2020-04-27 23:08:22 -0400

    added more post and put

--- quiz-backend/Controllers/QuestionsController.cs
+++ quiz-backend/Controllers/QuestionsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using quiz_backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace quiz_backend.Controllers
 {
@@ -13,16 +14,42 @@ namespace quiz_backend.Controllers
     [ApiController]
     public class QuestionsController : ControllerBase
     {
-        [HttpPost]
-        public void Post([FromBody]Question question)
+        readonly QuizContext context;
+        public QuestionsController(QuizContext context) 
         {
-            
+            this.context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Question question)
+        {
+            context.Add(question);
+            await context.SaveChangesAsync();
+            return Ok(question);
         }
 
         [HttpGet]
         public IEnumerable<Question> Get()
         {
-            return new Question[] { new Question(){ Text = "hello" }, new Question(){ Text = "hi" } };
+            return context.Questions;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody]Question question)
+        {
+            // Question question = await context.Questions.SingleOrDefaultAsync(q => q.ID == id);
+
+            //context.Entry needs ID in the body data, so to ensure id's are matching use a simple if statement to check
+            if(id != question.ID)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(question).State = EntityState.Modified;
+
+            await context.SaveChangesAsync();
+
+            return Ok(question);
         }
     }
 }
--- quiz-backend/Startup.cs
+++ quiz-backend/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace quiz_backend
 {
@@ -29,6 +30,8 @@ namespace quiz_backend
             {
                 builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
             }));
+
+            services.AddDbContext<QuizContext>(opt => opt.UseInMemoryDatabase("quiz"));
 
             services.AddControllers();
         }
